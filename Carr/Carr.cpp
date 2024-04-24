a26a32759cbf05b6af92e51481f78e859b832644
Nikol Nikolova <nnikolovanikol10@gmail.com>
2024-04-24 11:38:29 +0200

    Carr

--- Carr/Carr.cpp
+++ Carr/Carr.cpp
@@ -1,7 +1,112 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
+
+class Date {
+public:
+    int day;
+    int month;
+    int year;
+
+    Date() {
+        day = 1;
+        month = 1;
+        year = 1900;
+    }
+
+    Date(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+};
+
+class Address {
+public:
+    string country;
+    string region;
+    string city;
+    string street;
+    int houseNumber;
+    char buildingSection;
+
+    Address() {
+        country = "Not specified";
+        region = "Not specified";
+        city = "Not specified";
+        street = "Not specified";
+        houseNumber = 0;
+        buildingSection = 'A';
+    }
+
+    Address(string cn, string rg, string ct, string st, int hn, char bs) {
+        country = cn;
+        region = rg;
+        city = ct;
+        street = st;
+        houseNumber = hn;
+        buildingSection = bs;
+    }
+};
+
+class PersonalDetails {
+public:
+    string firstName;
+    string lastName;
+    Date* birthDate;
+    Address* homeAddress;
+
+    PersonalDetails() {
+        firstName = "";
+        lastName = "";
+        birthDate = new Date();
+        homeAddress = new Address();
+    }
+
+    void setDetails(const string& fn, const string& ln, Date* bd, Address* ha) {
+        firstName = fn;
+        lastName = ln;
+        birthDate = bd;
+        homeAddress = ha;
+    }
+
+    virtual void displayDetails() const {
+        cout << "Student Name: " << firstName << " " << lastName << endl;
+        cout << "Birth Date: " << birthDate->day << "/" << birthDate->month << "/" << birthDate->year << endl;
+        cout << "Address: " << homeAddress->street << ", " << homeAddress->city << ", " << homeAddress->country << endl;
+    }
+};
+
+class EducationDetails {
+public:
+    Date startDate;
+    int currentCourse;
+    string groupName;
+    string specialization;
+
+    EducationDetails() {
+        startDate = Date();
+        currentCourse = 1;
+        groupName = "None";
+        specialization = "None";
+    }
+
+    void setEducationDetails(const Date& sd, int cc, const string& gn, const string& sp) {
+        startDate = sd;
+        currentCourse = cc;
+        groupName = gn;
+        specialization = sp;
+    }
+
+    void displayEducation() const {
+        cout << "Start Date: " << startDate.day << "/" << startDate.month << "/" << startDate.year << endl;
+        cout << "Course: " << currentCourse << endl;
+        cout << "Group: " << groupName << endl;
+        cout << "Specialization: " << specialization << endl;
+    }
+};
 
 class Automobile {
 private:
@@ -11,58 +116,64 @@ private:
     int manufactureYear;
 
 public:
-    Automobile(string mk = "Unknown", string mdl = "Model", string clr = "Color", int yr = 1990)
-        : make(mk), model(mdl), color(clr), manufactureYear(yr) {}
+    Automobile() {
+        make = "Unknown";
+        model = "Model";
+        color = "Color";
+        manufactureYear = 1990;
+    }
 
-    // Setters with data validation
-    void updateMake(const string& mk) {
+    Automobile(string mk, string mdl, string clr, int yr) {
+        make = mk;
+        model = mdl;
+        color = clr;
+        manufactureYear = yr;
+    }
+
+    bool updateMake(const string& mk) {
         if (!mk.empty()) {
             make = mk;
+            return true;
         } else {
-            cout << "Invalid make. The make must not be empty." << endl;
+            cerr << "Invalid make. The make must not be empty." << endl;
+            return false;
         }
     }
 
-    void updateModel(const string& mdl) {
+    bool updateModel(const string& mdl) {
         if (!mdl.empty()) {
             model = mdl;
+            return true;
         } else {
-            cout << "Invalid model. The model must not be empty." << endl;
+            cerr << "Invalid model. The model must not be empty." << endl;
+            return false;
         }
     }
 
-    void updateColor(const string& clr) {
+    bool updateColor(const string& clr) {
         if (!clr.empty()) {
             color = clr;
+            return true;
         } else {
-            cout << "Invalid color. The color must not be empty." << endl;
+            cerr << "Invalid color. The color must not be empty." << endl;
+            return false;
         }
     }
 
-    void updateYear(int yr) {
-        if (yr > 1885) {  // Validating the year of manufacture (first car was made in 1886)
+    bool updateYear(int yr) {
+        if (yr > 1885) {
             manufactureYear = yr;
+            return true;
         } else {
-            cout << "Invalid year. The year must be later than 1885." << endl;
+            cerr << "Invalid year. The year must be later than 1885." << endl;
+            return false;
         }
     }
 
-    // Getters
-    string fetchMake() const {
-        return make;
-    }
-
-    string fetchModel() const {
-        return model;
-    }
-
-    string fetchColor() const {
-        return color;
-    }
-
-    int fetchYear() const {
-        return manufactureYear;
-    }
+    string fetchMake() const { return make; }
+    string fetchModel() const { return model; }
+    string fetchColor() const { return color; }
+    int fetchYear() const { return manufactureYear; }
 };
 
 class AutomobileDisplay {
@@ -93,20 +204,29 @@ public:
 
         cout << "Enter Make: ";
         getline(cin, mk);
-        automobile->updateMake(mk);
+        if (!automobile->updateMake(mk)) {
+            cout << "Using default make." << endl;
+        }
 
         cout << "Enter Model: ";
         getline(cin, mdl);
-        automobile->updateModel(mdl);
+        if (!automobile->updateModel(mdl)) {
+            cout << "Using default model." << endl;
+        }
 
         cout << "Enter Color: ";
         getline(cin, clr);
-        automobile->updateColor(clr);
+        if (!automobile->updateColor(clr)) {
+            cout << "Using default color." << endl;
+        }
 
         cout << "Enter Year: ";
-        cin >> yr;
-        automobile->updateYear(yr);
-        
+        if (!(cin >> yr) || !automobile->updateYear(yr)) {
+            cout << "Using default year." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
         cin.ignore(); // Consuming the leftover newline character
     }
 };
